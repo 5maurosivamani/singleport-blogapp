b73741dc8f0bad16f7d115ebb18fc2d4f6f2b73f
Sivamani <5maurosivamani@gmail.com>
2022-08-18 19:16:53 +0530

    added some client url cors changes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://fitness-blog-app-client.herokuapp.com/"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
