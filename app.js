9ebbb1c39f8d5a737a0c436b3b4b6b68b84a999a
Sivamani <5maurosivamani@gmail.com>
2022-08-18 19:25:10 +0530

    cors url changed

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["https://fitness-blog-app-client.herokuapp.com/"],
+    origin: ["https://fitness-blog-app-client.herokuapp.com"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
