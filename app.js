1545fddf2631f82fdfcc56d985b2edf37a012d9b
Sivamani <5maurosivamani@gmail.com>
2022-08-24 16:36:15 +0530

    changed the session configurations

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true, maxAge: 1000 * 60 * 60 * 24 },
+    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 },
   })
 );
 
