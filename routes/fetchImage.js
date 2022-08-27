93a486c53b84c0fb0201b9c52ceb34812c0f1127
Sivamani <5maurosivamani@gmail.com>
2022-08-27 16:31:58 +0530

    try to fix image issue

--- routes/fetchImage.js
+++ routes/fetchImage.js
@@ -5,9 +5,9 @@ const router = express.Router();
 router.get("/:img_name", (req, res) => {
   const pathArray = __dirname.split("/");
   const newPathArray = pathArray.splice(0, pathArray.length - 1);
-  const newPath = newPathArray.join("\\");
+  const newPath = newPathArray.join("/");
   var imagePath = newPath + "/public/images/" + req.params.img_name;
-  res.send(pathArray);
+  res.send(imagePath);
 });
 
 module.exports = router;
